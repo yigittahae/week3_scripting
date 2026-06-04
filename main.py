956469e8507929d3ee3d88430ed4e5d0b807f898
yigittahae <yigittahaeksi@gmail.com>
2026-06-04 14:27:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#https://freedict.org/freedict-database.json
+try:
+    f = open("ayasir.txt", "x")
+except:print("You already have a file named ", f.name ,"try another file name. ")
+
+for i in range (61):
+    with open("ayaz.txt", "a",encoding='utf-8') as f:
+        f.write("Ali Yasir ırmak " + "\n ")
+
+
+f= open("ayaz.txt",encoding='utf8')
+print(f.read())
